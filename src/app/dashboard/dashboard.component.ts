ba75af902f508d0c8d87310bf4c83c6c8730757f
Anita Yip <ceruleanox@gmail.com>
2021-02-21 00:05:57 -0500

    finished dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,5 +11,11 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  analyticsDashboard = [
+    {'id':1,'name':'Library','covid_index':'good','public_safety_index':'neutral','color_code':'green','image':'../../assets/dashboard-library.jpg'},
+    {'id':2,'name':'Gym','covid_index':'bad','public_safety_index':'neutral','color_code':'red','image':'../../assets/dashboard-gym.jpg'},
+    {'id':3,'name':'Dining Hall','covid_index':'good','public_safety_index':'bad','color_code':'yellow','image':'../../assets/dashboard-diningHall.jpg'},
+    {'id':4,'name':'Student Center','covid_index':'neutral','public_safety_index':'neutral','color_code':'yellow','image':'../../assets/dashboard-studentCenter.jpg'}
+  ]
 
 }
